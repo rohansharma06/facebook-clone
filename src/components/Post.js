03c3bb63ffb699baa5eee542b25650d9b6f19be1
rohansharma06 <rohansharma251998@gmail.com>
2020-08-02 06:47:12 +0530

    Add PostLike functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Comment } from './';
-import { createComment } from '../action/posts';
+import { createComment, addLikeToStore } from '../action/posts';
 
 class Post extends Component {
   constructor(props) {
@@ -36,9 +36,18 @@ class Post extends Component {
     });
   };
 
+  //---- method to dispatch action on liking a post
+  handlePostLike = () => {
+    const { post, user } = this.props;
+
+    this.props.dispatch(addLikeToStore(post._id, 'Post', user._id));
+  };
+
   render() {
-    const { post } = this.props;
+    const { post, user } = this.props;
     const { comment } = this.state;
+
+    const isPostLikedByUser = post.likes.includes(user._id);
 
     return (
       <div className="post-wrapper" key={post._id}>
@@ -58,13 +67,20 @@ class Post extends Component {
           <div className="post-content">{post.content}</div>
 
           <div className="post-actions">
-            <div className="post-like">
-              <img
-                src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
-                alt="likes-icon"
-              />
+            <button className="post-like no-btn" onClick={this.handlePostLike}>
+              {isPostLikedByUser ? (
+                <img
+                  src="https://image.flaticon.com/icons/svg/1076/1076984.svg"
+                  alt="like post"
+                />
+              ) : (
+                <img
+                  src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
+                  alt="likes-icon"
+                />
+              )}
               <span>{post.likes.length}</span>
-            </div>
+            </button>
 
             <div className="post-comments-icon">
               <img
@@ -98,4 +114,10 @@ Post.propTypes = {
   post: PropTypes.object.isRequired,
 };
 
-export default connect()(Post);
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+
+export default connect(mapStateToProps)(Post);
