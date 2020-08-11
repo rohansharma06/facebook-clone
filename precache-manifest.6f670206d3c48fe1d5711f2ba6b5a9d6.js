abf549c1eaccb2ff5001696836254be31399f9e0
rohansharma06 <rohansharma251998@gmail.com>
2020-08-11 11:57:47 +0530

    Updates

---
+++ precache-manifest.6f670206d3c48fe1d5711f2ba6b5a9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bdb154520294c23fe5b0c137074071",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "b7a72ff51b3e671c95f4",
+    "url": "/facebook-clone/static/css/main.56e6d0a2.chunk.css"
+  },
+  {
+    "revision": "6ca5dcbdd7d16cfa9263",
+    "url": "/facebook-clone/static/js/2.5f22f447.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/facebook-clone/static/js/2.5f22f447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a72ff51b3e671c95f4",
+    "url": "/facebook-clone/static/js/main.64680ce9.chunk.js"
+  },
+  {
+    "revision": "90ccc7202c3492ac96cc",
+    "url": "/facebook-clone/static/js/runtime-main.ce4f5c63.js"
+  }
+]);
